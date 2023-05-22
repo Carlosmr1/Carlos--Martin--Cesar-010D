594510daf6df12527e16cd6af30ca89db125a44b
Pasto247 <mar.cespedesg@duocuc.cl>
2023-05-22 18:33:47 -0400

    hola

--- the true GameHub/js/index.js
+++ the true GameHub/js/index.js
@@ -43,3 +43,12 @@ return false;
 
 
 } 
+
+const count = document.getElementById("contador-productos");
+const agregar = document.getElementById("agregar");
+let number = 0;
+
+agregar.addEventListener("click", ()=>{
+    number++;
+    count.innerHTML = number;
+})
