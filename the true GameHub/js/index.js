bf16169dbc37a46fd7425a490e9b8ff12f79b9ff
Carlosmr1 <106756931+Carlosmr1@users.noreply.github.com>
2023-05-23 16:43:19 -0400

    carritoweno

--- the true GameHub/js/index.js
+++ the true GameHub/js/index.js
@@ -1,38 +1,4 @@
 
-
-$('#submit').click(function() {
-
-    var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
-
-    if (regex.test($('#correo').val().trim())) {
-        
-    } else {
-        alert('La direccón de correo no es válida');
-    }
-    //validar usuario
-    if($('#user').val() === "" || $('#user').val()=== null){
-        alert("Debe ingresar un Nombre de usuario");
-        
-        return false;
-        }
-
-    // validar contraseña
-if ($('#psw').val() === "" || $('#psw').val() === null) {
-    alert("Debe ingresar una contraseña");
-    return false;
-}
-// validar confirmación de contraseña
-if ($('#confPsw').val() === "" || $('#confPsw').val() === null) {
-    alert("Debe confirmar la contraseña");
-    return false;
-    }
-    
-if ($('#psw').val() !== $('#confPsw').val() ) {
-alert("Las contraseñas no coinciden");
-return false;
-}
-
-});
 // function enviarFormulario()
 // {
 // var correo = document.getElementById("correo");
@@ -123,15 +89,6 @@ function agregarC(id){
     mostrarCarrito();
     console.log(carrito);
 }
-
-function carro(){
-    const contenedor = document.querySelector('.modal .modal-body');
-    console.log(contenedor);
-    carrito.forEach((prod) => {
-        const {id, nombre, precio} = prod;
-        contenedor.innerHTML= nombre;
-})}
-
 const contenedor = document.querySelector('.modal-content');
 const mostrarCarrito= ()=>{
     
@@ -156,10 +113,7 @@ function guardarCarrito(){
 }
 const carrito = []
 
-agregar.addEventListener("click", ()=>{
-    number++;
-    count.innerHTML = number;
-});
+
 
 
 var modal = document.getElementById("myModal");
