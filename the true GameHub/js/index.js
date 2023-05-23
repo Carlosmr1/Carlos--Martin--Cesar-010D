2cdf0fde93139ea6257739a4f8c645ee176f84ad
Pasto247 <mar.cespedesg@duocuc.cl>
2023-05-23 16:38:03 -0400

    commit

--- the true GameHub/js/index.js
+++ the true GameHub/js/index.js
@@ -1,38 +1,4 @@
 
-
-$('#submit').click(function() {
-
-    var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
-
-    if (regex.test($('#correo').val().trim())) {
-        
-    } else {
-        alert('La direccón de correo no es válida');
-    }
-    //validar usuario
-    if($('#user').val() === "" || $('#user').val()=== null){
-        alert("Debe ingresar un Nombre de usuario");
-        
-        return false;
-        }
-
-    // validar contraseña
-if ($('#psw').val() === "" || $('#psw').val() === null) {
-    alert("Debe ingresar una contraseña");
-    return false;
-}
-// validar confirmación de contraseña
-if ($('#confPsw').val() === "" || $('#confPsw').val() === null) {
-    alert("Debe confirmar la contraseña");
-    return false;
-    }
-    
-if ($('#psw').val() !== $('#confPsw').val() ) {
-alert("Las contraseñas no coinciden");
-return false;
-}
-
-});
 // function enviarFormulario()
 // {
 // var correo = document.getElementById("correo");
@@ -113,7 +79,7 @@ const listaProductos = [
 ];
 
 listaProductos.forEach((producto)=>{
-    const {id, nombre,precio} = producto
+    const {id, nombre,precio} = producto;
 });
 
 
@@ -135,8 +101,8 @@ function carro(){
 const contenedor = document.querySelector('.modal-content');
 const mostrarCarrito= ()=>{
     
-    carrito.forEach((producto) => {
-        const {id, nombre, precio} = producto;
+    carrito.forEach((prod) => {
+        const {id, nombre, precio} = prod;
 
         contenedor.innerHTML += '<p id="carro">'+nombre + " " + "$" + precio +"</p> <br>";
     })
@@ -197,3 +163,39 @@ function finalizarCompra(){
     number = 0;
     console.log(carrito);
 }
+
+
+
+$('#submit').click(function() {
+
+    var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
+
+    if (regex.test($('#correo').val().trim())) {
+        
+    } else {
+        alert('La direccón de correo no es válida');
+    }
+    //validar usuario
+    if($('#user').val() === "" || $('#user').val()=== null){
+        alert("Debe ingresar un Nombre de usuario");
+        
+        return false;
+        }
+
+    // validar contraseña
+if ($('#psw').val() === "" || $('#psw').val() === null) {
+    alert("Debe ingresar una contraseña");
+    return false;
+}
+// validar confirmación de contraseña
+if ($('#confPsw').val() === "" || $('#confPsw').val() === null) {
+    alert("Debe confirmar la contraseña");
+    return false;
+    }
+    
+if ($('#psw').val() !== $('#confPsw').val() ) {
+alert("Las contraseñas no coinciden");
+return false;
+}
+
+});
