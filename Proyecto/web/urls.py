debc2aee8cbf2036e68ba52ef4801df163ae81c5
Carlosmr1 <106756931+Carlosmr1@users.noreply.github.com>
2023-06-29 19:04:30 -0400

    links

--- Proyecto/web/urls.py
+++ Proyecto/web/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('',views.post_list, name='post_list'),
+    path('index',views.index, name='index'),
     path('juegos/', views.juegos, name='juegos'),
     path('juegosMesa', views.tableGames, name='juegosMesa'),
-
+    path('cuenta', views.cuenta, name='cuenta'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('newCuenta', views.newCuenta, name='newCuenta'),
 ]
