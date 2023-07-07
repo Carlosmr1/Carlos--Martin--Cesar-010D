0a84416c360a2edc110fa027e74cb94062c9da3d
Pasto247 <mar.cespedesg@duocuc.cl>
2023-07-07 18:30:42 -0400

    cARRITO fUncional

--- Proyecto/web/admin.py
+++ Proyecto/web/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from .models import Videogames, Marca, Tablegames, User
+from .models import Marca, Productos, User, Categoria
 
 
 # Register your models here.
 class ProductosAdmin(admin.ModelAdmin):
-    list_display = ["nombre", "precio", "marca"]
+    list_display = ["nombre", "precio", "marca","categoria"]
     list_editable = ["precio"]
     search_fields = ["nombre"]
     list_filter = ["marca"]
 
 admin.site.register(Marca)
-admin.site.register(Tablegames, ProductosAdmin )
-admin.site.register(Videogames, ProductosAdmin)
+admin.site.register(Productos, ProductosAdmin)
+admin.site.register(Categoria)
 admin.site.register(User)
